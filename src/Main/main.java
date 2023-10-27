5df96f6f50966931c1686264bdb1a3149cf5fb0a
Mixcoder03 <michdang.300103@gmail.com>
2023-10-27 19:00:09 +0700

    first commit

---
+++ src/Main/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import controller.ColletionWaste;
+
+/**
+ *
+ * @author admin
+ */
+public class main {
+    public static void main(String[] args) {
+        new ColletionWaste().run();
+    }
+}
